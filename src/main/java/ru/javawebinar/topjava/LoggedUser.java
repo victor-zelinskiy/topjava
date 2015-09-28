8c8b94ed4d7b9184a183474a6e52aa04e02f57ab
zilidev <zilian.nf@gmail.com>
2015-09-28 19:28:01 +0300

    add spring

---
+++ src/main/java/ru/javawebinar/topjava/LoggedUser.java
@@ -0,0 +1,16 @@
+package ru.javawebinar.topjava;
+
+/**
+ * GKislin
+ * 06.03.2015.
+ */
+public class LoggedUser {
+
+    public static int id() {
+        return 1;
+    }
+
+    public static int getCaloriesPerDay() {
+        return 2000;
+    }
+}
